ae3ed33c96a7413591dbc889c6fe63b06e5116e0
Navneet Singh <navneetsinghlovely07@gmail.com>
2024-04-07 21:56:37 +1000

    Update QNN.py

--- QNN.py
+++ QNN.py
@@ -17,7 +17,7 @@ from functools import partial
 # from qiskit import Aer
 from qiskit_machine_learning.circuit.library import RawFeatureVector
 from qiskit.primitives import Sampler
-
+from qiskit_machine_learning.circuit.library import QNNCircuit
 import json
 import time
 import warnings
@@ -34,6 +34,8 @@ from qiskit_algorithms.utils import algorithm_globals
 from sklearn.decomposition import PCA
 from qiskit_machine_learning.circuit.library import RawFeatureVector
 from qiskit_machine_learning.neural_networks import SamplerQNN
+
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
 
 algorithm_globals.random_seed = 42
 
@@ -59,6 +61,7 @@ def training_callback(weights, obj_func_eval):
         global itr
         itr += 1
         print(f"{itr} {obj_func_eval}", end=' | ')
+        print('\n')
 
 pca = PCA(n_components=8)  # for example, reduce to 2 dimensions
 train_sequences_pca = pca.fit_transform(train_sequences)
@@ -70,6 +73,48 @@ feature_dim = len(train_sequences_pca[0])
 
 from qiskit.circuit.library import ZZFeatureMap
 prep = ZZFeatureMap(feature_dim, reps=2, entanglement="full")
-prep.draw()
+# prep.draw()
 # prep = prep.assign_parameters(train_sequences_pca)
 ansatz = RealAmplitudes(num_qubits=feature_dim, reps=4)
+
+
+qc= QNNCircuit(num_qubits=feature_dim ,ansatz = ansatz, feature_map=prep)
+
+def parity(x):
+    return "{:b}".format(x).count("1") % 2
+output_shape = 2
+
+sampler_qnn = SamplerQNN(
+    circuit = qc,
+    interpret=parity,
+    output_shape=output_shape,
+    input_gradients= True
+)
+
+
+# def callback_graph(weights, obj_func_eval):
+#     clear_output(wait=True)
+#     objective_func_vals.append(obj_func_eval)
+#     plt.title("Objective function value against iteration")
+#     plt.xlabel("Iteration")
+#     plt.ylabel("Objective function value")
+#     plt.plot(range(len(objective_func_vals)), objective_func_vals)
+#     plt.show()
+
+sampler_classifier = NeuralNetworkClassifier(
+    neural_network=sampler_qnn, optimizer=COBYLA(maxiter=100), callback=training_callback
+)
+
+# fit classifier to data
+sampler_classifier.fit(train_sequences_pca,train_labels)
+
+# score classifier
+print("Score of the classifier:",sampler_classifier.score(test_sequences_pca,test_labels))
+
+
+
+
+
+
+
+
